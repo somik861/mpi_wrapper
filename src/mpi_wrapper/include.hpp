b935593dad32d452e7556731eafa915153070953
Jan <xjurack1@fi.muni.cz>
2022-09-27 16:55:09 +0200

    added TODO

--- src/mpi_wrapper/include.hpp
+++ src/mpi_wrapper/include.hpp
@@ -5,3 +5,19 @@
 #include "raii.hpp"
 #include "structs.hpp"
 #include "types.hpp"
+
+// TODO
+/*
+Comms: 
+MPI_Gather
+MPI_Scatter
+MPI_Reduce
+MPI_Allgather
+MPI_Allreduce
+MPI_Barrier
+
+Getters:
+MPI_Group_size
+MPI_Group_rank
+MPI_Get_processor_name
+*/
