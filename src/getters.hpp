dda35e91e2a25aee89e603f9ff4dd29e16334feb
Jan Juracka <xjurack1@fi.muni.cz>
2022-10-19 15:50:32 +0200

    added type size getter

--- src/getters.hpp
+++ src/getters.hpp
@@ -10,56 +10,64 @@ inline int Get_count(
     const MPI_Status& status,
     MPI_Datatype type,
     const std::source_location& location = std::source_location::current()) {
-    int count;
-    errors::check_code(MPI_Get_count(&status, type, &count), location);
-    return count;
+	int count;
+	errors::check_code(MPI_Get_count(&status, type, &count), location);
+	return count;
 }
 
 template <typename T>
 int Get_count(
     const MPI_Status& status,
     const std::source_location& location = std::source_location::current()) {
-    return Get_count(status, types::get_mpi_type<T>(), location);
+	return Get_count(status, types::get_mpi_type<T>(), location);
 }
 
 inline int Comm_rank(
     MPI_Comm comm,
     const std::source_location& location = std::source_location::current()) {
-    int rank;
-    errors::check_code(MPI_Comm_rank(comm, &rank), location);
-    return rank;
+	int rank;
+	errors::check_code(MPI_Comm_rank(comm, &rank), location);
+	return rank;
 }
 
 inline int Comm_size(
     MPI_Comm comm,
     const std::source_location& location = std::source_location::current()) {
-    int size;
-    errors::check_code(MPI_Comm_size(comm, &size), location);
-    return size;
+	int size;
+	errors::check_code(MPI_Comm_size(comm, &size), location);
+	return size;
 }
 
 inline int Group_rank(
     MPI_Group group,
     const std::source_location& location = std::source_location::current()) {
-    int rank;
-    errors::check_code(MPI_Group_rank(group, &rank), location);
-    return rank;
+	int rank;
+	errors::check_code(MPI_Group_rank(group, &rank), location);
+	return rank;
 }
 
 inline int Group_size(
     MPI_Group group,
     const std::source_location& location = std::source_location::current()) {
-    int size;
-    errors::check_code(MPI_Group_size(group, &size), location);
-    return size;
+	int size;
+	errors::check_code(MPI_Group_size(group, &size), location);
+	return size;
 }
 
 inline std::string Get_processor_name(
     const std::source_location& location = std::source_location::current()) {
-    std::array<char, MPI_MAX_PROCESSOR_NAME> name;
-    int count;
+	std::array<char, MPI_MAX_PROCESSOR_NAME> name;
+	int count;
 
-    errors::check_code(MPI_Get_processor_name(name.begin(), &count), location);
-    return std::string(name.begin(), name.begin() + count);
+	errors::check_code(MPI_Get_processor_name(name.begin(), &count), location);
+	return std::string(name.begin(), name.begin() + count);
+}
+
+inline int Type_size(
+    MPI_Datatype type,
+    const std::source_location& location = std::source_location::current()) {
+	int size;
+	errors::check_code(MPI_Type_size(type, &size), location);
+	return size;
 }
 } // namespace MPIw
